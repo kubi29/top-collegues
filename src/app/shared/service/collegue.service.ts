0da6e188211da4bf87fa018304e19069b0bc9056
Alex <a.peran@orange.fr>
2017-11-20 11:59:51 +0100

    environment

--- src/app/shared/service/collegue.service.ts
+++ src/app/shared/service/collegue.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Collegue } from '../domain/collegue';
-import {HttpClient} from '@angular/common/http';
-import {HttpHeaders} from '@angular/common/http';
-
+import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
   };
@@ -22,23 +22,23 @@ export class CollegueService {
     // TODO effectuer la liste des collègues
     // TODO retourner l'objet Promise<Collegue[]>
 
-     return this.http.get<Collegue[]>('http://localhost:8080/collegues').toPromise()
+     return this.http.get<Collegue[]>(environment.apiUrl + '/collegues').toPromise()
     }
     sauvegarder(newCollegue:Collegue):Promise<Collegue> {
     // TODO sauvegarder le nouveau collègue
     // TODO retourner l'objet Promise<Collegue[]>
       
-      return this.http.post<Collegue>('http://localhost:8080/collegues/', newCollegue,httpOptions).toPromise();
+      return this.http.post<Collegue>(environment.apiUrl + '/collegues/', newCollegue,httpOptions).toPromise();
     }
     aimerUnCollegue(unCollegue:Collegue):Promise<Collegue> {
     // TODO Aimer un collègue
     unCollegue.score += 10;
-     return this.http.put<Collegue>('http://localhost:8080/collegues/${unCollegue.nom}/score', unCollegue,httpOptions).toPromise()
+     return this.http.put<Collegue>(environment.apiUrl + '/collegues/${unCollegue.nom}/score', unCollegue,httpOptions).toPromise()
     }
     detesterUnCollegue(unCollegue:Collegue):Promise<Collegue> {
     // TODO Aimer un collègue
     unCollegue.score -= 5;
-    return this.http.put<Collegue>('http://localhost:8080/collegues/${unCollegue.nom}/score', unCollegue,httpOptions).toPromise()
+    return this.http.put<Collegue>(environment.apiUrl + '/collegues/${unCollegue.nom}/score', unCollegue,httpOptions).toPromise()
   }
 
 }
